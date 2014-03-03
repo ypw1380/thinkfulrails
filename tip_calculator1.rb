8c566e1fee83c13c090d7c7d7c31cf82c2cdf355
ypw1380 <ypw1380@gmail.com>
2014-03-03 15:42:08 +0000

    modify and amend

--- tip_calculator1.rb
+++ tip_calculator1.rb
@@ -9,6 +9,6 @@ total_cost = meal_with_tax + tip_value
 
 
 print "The pre-tax cost of your meal was $%.2f.\n" % meal
-print "At %d%%, tax for this meal is $%.2f.\n" % [tax, meal_with_tax]
-print "For a %d%% tip, you should leave $%.2f.\n" % [tip, tip_value]
+print "At %d, tax for this meal is $%.2f.\n" % [tax, meal_with_tax]
+print "For a %d tip, you should leave $%.2f.\n" % [tip, tip_value]
 print "The grand total for this meal is then $%.2f.\n" % total_cost
