15e8c9b4db42ff81275e93ccd809e15291b86fee
ypw1380 <ypw1380@gmail.com>
2014-03-03 15:43:08 +0000

    adding second file

---
+++ tip_calculator2.rb
@@ -0,0 +1,16 @@
+print "Enter the cost of your meal: "
+meal = Float(gets)
+print "What percent tax would you like to leave: "
+tax = Float(gets)
+print "How much tax would you like to leave: "
+tip = Float(gets)
+
+tax_value = meal * tax/100.0
+meal_with_tax = (meal + tax_value)
+tip_value = meal * tip/100.0
+total_cost = meal_with_tax + tip_value
+
+print "The pre-tax cost of your meal was $%.2f.\n" % meal
+print "At %d%%, tax for this meal is $%.2f.\n" % [tax, tax_value]
+print "For a %d%% tip, you should leave $%.2f.\n" % [tip, tip_value]
+print "The grand total for this meal is then $%.2f.\n" % total_cost
